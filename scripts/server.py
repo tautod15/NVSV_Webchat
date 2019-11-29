ad86771f663f676a7b65003bfb113820e4a44fe4
Tausendschoen Tobias <tautod15@htlkaindorf.at>
2019-11-29 11:36:34 +0100

    webcam pfunsch ding

--- scripts/server.py
+++ scripts/server.py
@@ -1,25 +1,34 @@
 import asyncio
+import base64
 import datetime
 import random
+
+import cv2
 import websockets
 
 USERS = set()
 USERDICT = {}
-
+cap = cv2.VideoCapture(0)
 async def register(websocket):
     USERS.add(websocket)
     USERDICT[websocket] = str(len(USERDICT) + 1)
+
+async def render_webcamb(websocket):
+    ret, frame = cap.read()
+    ret, jpgBuffer = cv2.imencode('.jpg', frame)
+    b64jpg = base64.b64encode(jpgBuffer)
+    b64 = str(b64jpg)[2:-1]
+    await websocket.send(b64)
 
 async def time(websocket, path):
     await register(websocket)
 
     while True:
-        now = datetime.datetime.utcnow().isoformat() + "Z"
-
-        message = await websocket.recv()
+        message = "" #await websocket.recv()
         if message is not None and USERS:
             await asyncio.wait([user.send(USERDICT[websocket] + ": " + message) for user in USERS])
         await asyncio.sleep(0.25)
+        await render_webcamb(websocket)
 
 
 
